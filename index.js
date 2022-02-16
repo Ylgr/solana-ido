feb3a808c38c9147c9e7998964181fd812097e18
ylgr <quyen.luminous@gmail.com>
2022-02-16 20:59:50 +0700

    (+) buy request

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const anchor = require("@project-serum/anchor");
+const anchor = require('@project-serum/anchor');
 const { web3, Provider, Wallet, utils } = anchor;
 const { Account, Transaction, SystemProgram, PublicKey } = web3;
 const Base58 = require('base-58');
-const { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID, Token } = require("@solana/spl-token");
+const { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID, Token } = require('@solana/spl-token');
 const BN = anchor.BN;
 require('dotenv').config();
 
-const connection = new web3.Connection(process.env.NETWORK === "LOCAL" ? "http://127.0.0.1:8899/":"https://api.devnet.solana.com/");
+const connection = new web3.Connection(process.env.NETWORK === 'LOCAL' ? 'http://127.0.0.1:8899/':'https://api.devnet.solana.com/');
 
 const testFeePayerSecretKey = 'EGeG7Q6j8DedNW1ayFJW6a1eqUYCVobJ3CkKMxGPUb3vZLfHJhYDy2YrY8bwrNy2kZvqRPduFn8KXUUxJtqEPB3'
 const testMasterSecretKey = '5rfnMoHXULH1fvcvdeBNwNLCJjDrNjd5UoqQZiNxzxJ1DFXTAWj8HRJNzxh4kaoeSnjZvaGZ8yLYAvuX7W1SEfxQ'
@@ -25,18 +25,21 @@ const bicTokenPublicKey = new web3.PublicKey('9kJAfNMhZNtaTQTMGvrnRnCg1qxeCunFdv
 const birTokenPublicKey = new web3.PublicKey('2EVyXs7AM8tgkjweRFkwCDe8DaVEYetDPG2EauAu2U6B')
 const bgtTokenPublicKey = new web3.PublicKey('3CvHRa7W8KcQG37xEqvUoUwNonxVBMkPRNttHn9MVczg')
 const usdtTokenPublicKey = new web3.PublicKey('v2aShdenyX3bNDvUWzRYBXPC5yeuhtBjTTg3AiF5TAE')
+const DECIMALS = 6;
 
-const currentPrice = 0;
-const coreTeamRole = 'CORE_TEAM_ROLE';
-const communityRole = 'COMMUNITY_ROLE';
-const whitelist = {
-    '2B8SUxUHwUMCaGBR564L5KLDGJ7SyjbZDzXZifbvrhdv': coreTeamRole,
-    '6qbhYEGCMihaQiRt66oTMDgvCm2VY23vJsETGN6rs8z1': communityRole,
+const currentBicPrice = 0.1;
+const birReceiveRate = 0.33;
+const bgtReceiveRate = 1;
+const bicRefRate = 0.1;
+const birRefRate = 0.03;
+const bgtRefRate = 0.1;
+const ref = {
+    '2B8SUxUHwUMCaGBR564L5KLDGJ7SyjbZDzXZifbvrhdv': new web3.PublicKey('6qbhYEGCMihaQiRt66oTMDgvCm2VY23vJsETGN6rs8z1'),
+    '6qbhYEGCMihaQiRt66oTMDgvCm2VY23vJsETGN6rs8z1': new web3.PublicKey('6qbhYEGCMihaQiRt66oTMDgvCm2VY23vJsETGN6rs8z1'),
 }
-const priceForRentAccountInUsdt = 0;
+const amountUsdtForRentAccount = 0.2 * Math.pow(10, DECIMALS);
 
 const provider = new Provider(connection, testFeePayerWallet, {skipPreflight: false});
-const DECIMALS = 6;
 
 async function createTokensAndMintIt() {
     const amountMint = 10000*1e6;
@@ -154,9 +157,223 @@ async function transferUsdtToUser(amountUsdt) {
     logOnExplorer(receipt);
 }
 
-transferUsdtToUser(1);
+async function getListAssociatedAccount(publicKey) {
+    const usdt = await Token.getAssociatedTokenAddress(
+        ASSOCIATED_TOKEN_PROGRAM_ID,
+        TOKEN_PROGRAM_ID,
+        usdtTokenPublicKey,
+        publicKey
+    )
+    const bic = await Token.getAssociatedTokenAddress(
+        ASSOCIATED_TOKEN_PROGRAM_ID,
+        TOKEN_PROGRAM_ID,
+        bicTokenPublicKey,
+        publicKey
+    )
+    const bir = await Token.getAssociatedTokenAddress(
+        ASSOCIATED_TOKEN_PROGRAM_ID,
+        TOKEN_PROGRAM_ID,
+        birTokenPublicKey,
+        publicKey
+    )
+    const bgt = await Token.getAssociatedTokenAddress(
+        ASSOCIATED_TOKEN_PROGRAM_ID,
+        TOKEN_PROGRAM_ID,
+        bgtTokenPublicKey,
+        publicKey
+    )
+    return {bic, bir, bgt, usdt}
+}
 
 async function createBuyRequest(amountUsdt) {
-    const amountDecimal = amountUsdt*Math.pow(10, DECIMALS);
+    amountUsdt = amountUsdt*Math.pow(10, DECIMALS);
+    const userPublicKey = user1Wallet.publicKey;
+    const userPayer = user1Wallet.payer;
 
+    const masterAssociatedAccount = await getListAssociatedAccount(testMasterWallet.publicKey);
+    const userAssociatedAccount = await getListAssociatedAccount(userPublicKey);
+
+    const refUserPublicKey = ref[userPublicKey]
+    if(!refUserPublicKey) {
+        console.log('User not whitelist and ref!')
+        return
+    }
+    const refAssociatedAccount = await getListAssociatedAccount(refUserPublicKey);
+
+    const instructionRentAccount = []
+
+    // BIC user
+    try {
+        await connection.getTokenAccountBalance(userAssociatedAccount.bic)
+    } catch (e) {
+        amountUsdt-=amountUsdtForRentAccount;
+        if(amountUsdt < 0) {
+            console.log('Not enough USDT for rent!')
+            return
+        }
+        instructionRentAccount.push(Token.createAssociatedTokenAccountInstruction(
+            ASSOCIATED_TOKEN_PROGRAM_ID,
+            TOKEN_PROGRAM_ID,
+            bicTokenPublicKey,
+            userAssociatedAccount.bic,
+            userPublicKey,
+            testFeePayerWallet.publicKey
+        ))
+    }
+
+    // BIR user
+    try {
+        await connection.getTokenAccountBalance(userAssociatedAccount.bir)
+    } catch (e) {
+        amountUsdt-=amountUsdtForRentAccount;
+        if(amountUsdt < 0) {
+            console.log('Not enough USDT for rent!')
+            return
+        }
+        instructionRentAccount.push(Token.createAssociatedTokenAccountInstruction(
+            ASSOCIATED_TOKEN_PROGRAM_ID,
+            TOKEN_PROGRAM_ID,
+            birTokenPublicKey,
+            userAssociatedAccount.bir,
+            userPublicKey,
+            testFeePayerWallet.publicKey
+        ))
+    }
+
+    // BGT user
+    try {
+        await connection.getTokenAccountBalance(userAssociatedAccount.bgt)
+    } catch (e) {
+        amountUsdt-=amountUsdtForRentAccount;
+        if(amountUsdt < 0) {
+            console.log('Not enough USDT for rent!')
+            return
+        }
+        instructionRentAccount.push(Token.createAssociatedTokenAccountInstruction(
+            ASSOCIATED_TOKEN_PROGRAM_ID,
+            TOKEN_PROGRAM_ID,
+            bgtTokenPublicKey,
+            userAssociatedAccount.bgt,
+            userPublicKey,
+            testFeePayerWallet.publicKey
+        ))
+    }
+
+    // BIC Ref
+    try {
+        await connection.getTokenAccountBalance(refAssociatedAccount.bic)
+    } catch (e) {
+        instructionRentAccount.push(Token.createAssociatedTokenAccountInstruction(
+            ASSOCIATED_TOKEN_PROGRAM_ID,
+            TOKEN_PROGRAM_ID,
+            bicTokenPublicKey,
+            refAssociatedAccount.bic,
+            refUserPublicKey,
+            testFeePayerWallet.publicKey
+        ))
+    }
+
+    // BIR Ref
+    try {
+        await connection.getTokenAccountBalance(refAssociatedAccount.bir)
+    } catch (e) {
+        instructionRentAccount.push(Token.createAssociatedTokenAccountInstruction(
+            ASSOCIATED_TOKEN_PROGRAM_ID,
+            TOKEN_PROGRAM_ID,
+            birTokenPublicKey,
+            refAssociatedAccount.bir,
+            refUserPublicKey,
+            testFeePayerWallet.publicKey
+        ))
+    }
+
+    // BGT Ref
+    try {
+        await connection.getTokenAccountBalance(refAssociatedAccount.bgt)
+    } catch (e) {
+        instructionRentAccount.push(Token.createAssociatedTokenAccountInstruction(
+            ASSOCIATED_TOKEN_PROGRAM_ID,
+            TOKEN_PROGRAM_ID,
+            bgtTokenPublicKey,
+            refAssociatedAccount.bgt,
+            refUserPublicKey,
+            testFeePayerWallet.publicKey
+        ))
+    }
+
+    const bicReceive = amountUsdt*currentBicPrice;
+    const birReceive = bicReceive*birReceiveRate;
+    const bgtReceive = bicReceive*bgtReceiveRate;
+    const bicRef = bicReceive*bicRefRate;
+    const birRef = bicReceive*birRefRate;
+    const bgtRef = bicReceive*bgtRefRate;
+    const buyAndRewardInstructions = [
+        Token.createTransferInstruction(
+            TOKEN_PROGRAM_ID,
+            userAssociatedAccount.usdt,
+            masterAssociatedAccount.usdt,
+            userPublicKey,
+            [],
+            amountUsdt
+        ),
+        Token.createTransferInstruction(
+            TOKEN_PROGRAM_ID,
+            masterAssociatedAccount.bic,
+            userAssociatedAccount.bic,
+            testMasterWallet.publicKey,
+            [],
+            bicReceive
+        ),
+        Token.createTransferInstruction(
+            TOKEN_PROGRAM_ID,
+            masterAssociatedAccount.bir,
+            userAssociatedAccount.bir,
+            testMasterWallet.publicKey,
+            [],
+            birReceive
+        ),
+        Token.createTransferInstruction(
+            TOKEN_PROGRAM_ID,
+            masterAssociatedAccount.bgt,
+            userAssociatedAccount.bgt,
+            testMasterWallet.publicKey,
+            [],
+            bgtReceive
+        ),
+        Token.createTransferInstruction(
+            TOKEN_PROGRAM_ID,
+            masterAssociatedAccount.bic,
+            refAssociatedAccount.bic,
+            testMasterWallet.publicKey,
+            [],
+            bicRef
+        ),
+        Token.createTransferInstruction(
+            TOKEN_PROGRAM_ID,
+            masterAssociatedAccount.bir,
+            refAssociatedAccount.bir,
+            testMasterWallet.publicKey,
+            [],
+            birRef
+        ),
+        Token.createTransferInstruction(
+            TOKEN_PROGRAM_ID,
+            masterAssociatedAccount.bgt,
+            refAssociatedAccount.bgt,
+            testMasterWallet.publicKey,
+            [],
+            bgtRef
+        ),
+    ]
+    const instructions = instructionRentAccount.concat(buyAndRewardInstructions)
+
+    let tx = new web3.Transaction().add(...instructions)
+    tx.feePayer = testFeePayerWallet.payer.publicKey
+    tx.recentBlockhash = (await connection.getRecentBlockhash()).blockhash
+    tx.partialSign(testFeePayerWallet.payer)
+    tx.partialSign(testMasterWallet.payer)
+    tx.partialSign(userPayer)
+    const rawTx = tx.serialize()
+    const receipt = await connection.sendRawTransaction(rawTx)
+    logOnExplorer(receipt);
 }
